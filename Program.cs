1075d60c8f31cc4ccef2b019dd8ce63bf6293a49
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-17 14:37:39 +0200

    add quandrato function

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
 ﻿using System;
-public class ArrayExample
+//public class ArrayExample
+//{
+//    static void printArray(int[] arr)
+//    {
+//        Console.WriteLine("Printing array elements:");
+//        for (int i = 0; i < arr.Length; i++)
+//        {
+//            Console.WriteLine(arr[i]);
+//        }
+//    }
+//    public static void Main(string[] args)
+//    {
+//        int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+        
+//        printArray(arr1);//passing array to function  
+        
+//    }
+//}
+
+
+
+public class Program
 {
-    static void printArray(int[] arr)
+    public static void Quandrato()
     {
-        Console.WriteLine("Printing array elements:");
-        for (int i = 0; i < arr.Length; i++)
-        {
-            Console.WriteLine(arr[i]);
-        }
-    }
-    public static void Main(string[] args)
-    {
-        int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+        double Result, Number1;
+
+        Number1 = 3;
         
-        printArray(arr1);//passing array to function  
-        
+
+        Result = Math.Pow(Number1, 2);
+        Console.WriteLine(Result);
     }
 }
