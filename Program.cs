61d60b30edde68187120cde682d2af8842f4fe93
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-17 14:27:36 +0200

    add printArray funcontion

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class ArrayExample
+{
+    static void printArray(int[] arr)
+    {
+        Console.WriteLine("Printing array elements:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] arr1 = { 25, 10, 20, 15, 40, 50 };
+        
+        printArray(arr1);//passing array to function  
+        
+    }
+}
