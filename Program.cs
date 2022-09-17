1a4909019f9aa2b159d8b892d25ffbe1a7005afc
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-17 15:47:32 +0200

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,40 +1,4 @@
-﻿//using System;
-//public class ArrayExample
-//{
-//   static void printArray(int[] arr)
-//   {
-//       Console.WriteLine("Printing array elements:");
-//       for (int i = 0; i < arr.Length; i++)
-//       {
-//           Console.WriteLine(arr[i]);
-//       }
-//   }
-//   public static void Main(string[] args)
-//   {
-//       int[] arr1 = { 25, 10, 20, 15, 40, 50 };
-
-//       printArray(arr1);//passing array to function  
-
-//   }
-//}
-
-
-
-//public class Program
-//{
-//    public static void Quandrato()
-//    {
-//        double Result, Number1;
-
-//        Number1 = 3;
-
-
-//        Result = Math.Pow(Number1, 2);
-//        Console.WriteLine(Result);
-//    }
-//}
-
-//1
+﻿
 using System.Drawing;
 
 void stampaArray(int[] array)
